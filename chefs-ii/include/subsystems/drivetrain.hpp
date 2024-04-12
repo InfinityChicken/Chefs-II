eb0603719a7569e834d90d7722931cdb6905d25b
Infinity Chicken <117489481+InfinityChicken@users.noreply.github.com>
2024-04-12 16:41:58 -0700

    drivecode start

---
+++ chefs-ii/include/subsystems/drivetrain.hpp
@@ -0,0 +1,10 @@
+#include "main.h";
+
+extern const int leftFront;
+extern const int leftMid;
+extern const int leftBack;
+extern const int rightFront;
+extern const int rightMid;
+extern const int rightBack;
+
+extern std::shared_ptr<
