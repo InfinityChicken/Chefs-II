b559adef76be0781b86a241277ebc0b74baa0087
Infinity Chicken <117489481+InfinityChicken@users.noreply.github.com>
2024-04-12 17:59:13 -0700

    extra stuff

--- src/subsystems/drivetrain.cpp
+++ src/subsystems/drivetrain.cpp
@@ -5,4 +5,5 @@ extern std::shared_ptr<okapi::ChassisController> drivetrain =
 okapi::ChassisControllerBuilder()
 .withMotors({leftBack, leftMid, leftFront}, {rightBack, rightMid, rightFront})
 .withDimensions(okapi::AbstractMotor::gearset::green, {{1_in, 1_in}, okapi::imev5GreenTPR}) //TODO: find actual drive measurements
+// .withSensors(leftMid.getEncoder(), rightMid.getEncoder()); TODO: make this work bc we pass in motor integers but we need to pass in motor objects
 .build();
