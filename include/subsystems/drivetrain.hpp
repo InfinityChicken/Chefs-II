d71c4364a7d06431c144d48c2b36387bb543ab1b
Jiayi Zhang <jz5883@pleasantonusd.net>
2024-04-13 19:41:22 -0700

    added comments

--- include/subsystems/drivetrain.hpp
+++ include/subsystems/drivetrain.hpp
@@ -1,11 +1,16 @@
-#include "main.h";
+#include "main.h"       //no semicolon should be here
 
 //ports are arbitrary, just make sure that the signs stay the same
-extern const int8_t leftFront = 1;
-extern const int8_t leftMid = 2;
-extern const int8_t leftBack = 3;
-extern const int8_t rightFront = -4;
-extern const int8_t rightMid = -5;
-extern const int8_t rightBack = -6;
 
-extern std::shared_ptr<okapi::ChassisController> drivetrain;
+const int8_t leftFront = 0;     //don't assign any values to an extern statement
+const int8_t leftMid = 0;      
+const int8_t leftBack = 0;      //(technically you should *define* and assign any variable in a src file, it's better
+const int8_t rightFront = 0;    //origanization and the compiler won't be forced to recompile every single file that
+const int8_t rightMid = 0;      //this header is linked to.) 
+const int8_t rightBack = 0;     //(it's better to use constexpr or define in this case but don't worry about that rn.)
+
+//About the LOG_INFO stuff, just ignore it. The compiler is saying that there is a small risk of 
+//the program crashing if you capture class pointer lambda function arguments using copy, but that's 
+//only if we using multithreading and we are not doing that right now
+
+extern std::shared_ptr<okapi::ChassisController> drivetrain;    //good
--- src/subsystems/drivetrain.cpp
+++ src/subsystems/drivetrain.cpp
@@ -1,7 +1,11 @@
-#include "subsystems/drivetrain.hpp";
+#include "subsystems/drivetrain.hpp"    //no semicolon at end of #include statements
+
 using namespace okapi::literals;
 
-extern std::shared_ptr<okapi::ChassisController> drivetrain = 
+//do not write extern here, as you are constructing this variable in this file
+//extern should only be used in header files (although there are some exceptions)
+ 
+std::shared_ptr<okapi::ChassisController> drivetrain = 
 okapi::ChassisControllerBuilder()
 .withMotors({leftBack, leftMid, leftFront}, {rightBack, rightMid, rightFront})
 .withDimensions(okapi::AbstractMotor::gearset::green, {{1_in, 1_in}, okapi::imev5GreenTPR}) //TODO: find actual drive measurements
